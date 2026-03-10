e1cce3c91d154b3287ee612846565a4fd0efe8c1
fabricioomarques-ctrl <fabricioo.marques@gmail.com>
2026-03-10 04:49:05 -0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,14 +1,39 @@
 import os
+import requests
+from bs4 import BeautifulSoup
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 
 TOKEN = os.environ["TELEGRAM_TOKEN"]
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("Bot funcionando 🚀")
+    await update.message.reply_text(
+        "✈️ Monitor de Milhas Ativo!\n\n"
+        "Comandos:\n"
+        "/promocoes - Buscar promoções de milhas"
+    )
+
+async def promocoes(update: Update, context: ContextTypes.DEFAULT_TYPE):
+
+    url = "https://www.melhoresdestinos.com.br/tag/milhas"
+
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, "html.parser")
+
+    posts = soup.select("h2.entry-title a")
+
+    resposta = "🔥 Promoções encontradas:\n\n"
+
+    for post in posts[:5]:
+        titulo = post.text
+        link = post["href"]
+        resposta += f"{titulo}\n{link}\n\n"
+
+    await update.message.reply_text(resposta)
 
 app = ApplicationBuilder().token(TOKEN).build()
 
 app.add_handler(CommandHandler("start", start))
+app.add_handler(CommandHandler("promocoes", promocoes))
 
 app.run_polling()
