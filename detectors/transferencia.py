b72064c0133f18226969470d9e57361c0317adf5
fabricioomarques-ctrl <fabricioo.marques@gmail.com>
2026-03-13 01:06:14 -0300

    Update transferencia.py

--- detectors/transferencia.py
+++ detectors/transferencia.py
@@ -1,1 +1,26 @@
+from utils.texto import normalizar_texto
 
+
+KEYWORDS_TRANSFERENCIA = [
+
+    "transferencia bonificada",
+    "bonus de transferencia",
+    "bônus de transferência",
+
+    "transferir pontos",
+    "transfira seus pontos",
+
+    "bonificada",
+]
+
+
+def detectar_transferencia(texto):
+
+    texto = normalizar_texto(texto)
+
+    for palavra in KEYWORDS_TRANSFERENCIA:
+
+        if palavra in texto:
+            return True
+
+    return False
