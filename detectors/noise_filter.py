95827b3dc283adf9c20012fa7e79c1745cd0c874
fabricioomarques-ctrl <fabricioo.marques@gmail.com>
2026-03-13 02:41:42 -0300

    Update noise_filter.py

--- detectors/noise_filter.py
+++ detectors/noise_filter.py
@@ -1,1 +1,12 @@
+from config import NOISE_WORDS
+from utils.texto import normalizar_texto
 
+
+def is_noise(texto):
+    texto = normalizar_texto(texto)
+
+    for palavra in NOISE_WORDS:
+        if normalizar_texto(palavra) in texto:
+            return True
+
+    return False
