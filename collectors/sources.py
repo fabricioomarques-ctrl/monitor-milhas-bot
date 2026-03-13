64ca8bcfc08aea4e5121cc9ea27eddbe8af13670
fabricioomarques-ctrl <fabricioo.marques@gmail.com>
2026-03-13 20:02:46 -0300

    Create sources.py

---
+++ collectors/sources.py
@@ -0,0 +1,101 @@
+FONTES = [
+    # BLOGS RSS
+    {
+        "name": "Pontos pra Voar Feed",
+        "type": "rss",
+        "url": "https://pontospravoar.com/feed",
+        "group": "blogs",
+    },
+    {
+        "name": "Passageiro de Primeira Feed",
+        "type": "rss",
+        "url": "https://passageirodeprimeira.com/feed",
+        "group": "blogs",
+    },
+    {
+        "name": "Melhores Destinos Feed",
+        "type": "rss",
+        "url": "https://www.melhoresdestinos.com.br/feed",
+        "group": "blogs",
+    },
+    {
+        "name": "AEROIN Feed",
+        "type": "rss",
+        "url": "https://www.aeroin.net/feed",
+        "group": "blogs",
+    },
+
+    # PÁGINAS/CATEGORIAS PPV
+    {
+        "name": "PPV LATAM Pass",
+        "type": "html",
+        "url": "https://pontospravoar.com/category/programas-de-fidelidade/latam-pass/",
+        "group": "programas",
+    },
+    {
+        "name": "PPV Smiles",
+        "type": "html",
+        "url": "https://pontospravoar.com/category/programas-de-fidelidade/smiles/",
+        "group": "programas",
+    },
+    {
+        "name": "PPV Esfera",
+        "type": "html",
+        "url": "https://pontospravoar.com/category/programas-de-fidelidade/esfera/",
+        "group": "programas",
+    },
+    {
+        "name": "PPV Azul Fidelidade",
+        "type": "html",
+        "url": "https://pontospravoar.com/category/programas-de-fidelidade/azul-fidelidade/",
+        "group": "programas",
+    },
+    {
+        "name": "PPV Home",
+        "type": "html",
+        "url": "https://pontospravoar.com/",
+        "group": "redes_sociais",
+    },
+
+    # FONTES OFICIAIS IMPORTANTES
+    {
+        "name": "LATAM Pass Oficial",
+        "type": "html",
+        "url": "https://latampass.latam.com/pt_br/",
+        "group": "programas",
+        "fallback_urls": [
+            "https://www.latamairlines.com/br/pt/ofertas",
+        ],
+    },
+    {
+        "name": "Smiles Oficial",
+        "type": "html",
+        "url": "https://www.smiles.com.br/",
+        "group": "programas",
+    },
+    {
+        "name": "Esfera Oficial",
+        "type": "html",
+        "url": "https://www.esfera.com.vc/",
+        "group": "programas",
+    },
+    {
+        "name": "TudoAzul Oficial",
+        "type": "html",
+        "url": "https://www.tudoazul.com/",
+        "group": "programas",
+        "optional": True,
+    },
+    {
+        "name": "Livelo Oficial",
+        "type": "html",
+        "url": "https://www.livelo.com.br/",
+        "group": "programas",
+    },
+    {
+        "name": "MaxMilhas Oficial",
+        "type": "html",
+        "url": "https://www.maxmilhas.com.br/",
+        "group": "milheiro",
+    },
+]
