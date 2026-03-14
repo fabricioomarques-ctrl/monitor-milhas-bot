1d2304b4dbebe83a0dbf4f3455964c870f99005e
fabricioomarques-ctrl <fabricioo.marques@gmail.com>
2026-03-13 22:07:13 -0300

    Update sources.py

--- collectors/sources.py
+++ collectors/sources.py
@@ -1,101 +1,16 @@
-FONTES = [
-    # BLOGS RSS
-    {
-        "name": "Pontos pra Voar Feed",
-        "type": "rss",
-        "url": "https://pontospravoar.com/feed",
-        "group": "blogs",
-    },
-    {
-        "name": "Passageiro de Primeira Feed",
-        "type": "rss",
-        "url": "https://passageirodeprimeira.com/feed",
-        "group": "blogs",
-    },
-    {
-        "name": "Melhores Destinos Feed",
-        "type": "rss",
-        "url": "https://www.melhoresdestinos.com.br/feed",
-        "group": "blogs",
-    },
-    {
-        "name": "AEROIN Feed",
-        "type": "rss",
-        "url": "https://www.aeroin.net/feed",
-        "group": "blogs",
-    },
+# Fontes RSS monitoradas pelo Radar de Milhas
 
-    # PÁGINAS/CATEGORIAS PPV
-    {
-        "name": "PPV LATAM Pass",
-        "type": "html",
-        "url": "https://pontospravoar.com/category/programas-de-fidelidade/latam-pass/",
-        "group": "programas",
-    },
-    {
-        "name": "PPV Smiles",
-        "type": "html",
-        "url": "https://pontospravoar.com/category/programas-de-fidelidade/smiles/",
-        "group": "programas",
-    },
-    {
-        "name": "PPV Esfera",
-        "type": "html",
-        "url": "https://pontospravoar.com/category/programas-de-fidelidade/esfera/",
-        "group": "programas",
-    },
-    {
-        "name": "PPV Azul Fidelidade",
-        "type": "html",
-        "url": "https://pontospravoar.com/category/programas-de-fidelidade/azul-fidelidade/",
-        "group": "programas",
-    },
-    {
-        "name": "PPV Home",
-        "type": "html",
-        "url": "https://pontospravoar.com/",
-        "group": "redes_sociais",
-    },
+FONTES_RSS = [
 
-    # FONTES OFICIAIS IMPORTANTES
-    {
-        "name": "LATAM Pass Oficial",
-        "type": "html",
-        "url": "https://latampass.latam.com/pt_br/",
-        "group": "programas",
-        "fallback_urls": [
-            "https://www.latamairlines.com/br/pt/ofertas",
-        ],
-    },
-    {
-        "name": "Smiles Oficial",
-        "type": "html",
-        "url": "https://www.smiles.com.br/",
-        "group": "programas",
-    },
-    {
-        "name": "Esfera Oficial",
-        "type": "html",
-        "url": "https://www.esfera.com.vc/",
-        "group": "programas",
-    },
-    {
-        "name": "TudoAzul Oficial",
-        "type": "html",
-        "url": "https://www.tudoazul.com/",
-        "group": "programas",
-        "optional": True
-    },
-    {
-        "name": "Livelo Oficial",
-        "type": "html",
-        "url": "https://www.livelo.com.br/",
-        "group": "programas",
-    },
-    {
-        "name": "MaxMilhas Oficial",
-        "type": "html",
-        "url": "https://www.maxmilhas.com.br/",
-        "group": "milheiro",
-    },
+    # Sites principais do mercado de milhas
+    "https://passageirodeprimeira.com/feed",
+    "https://pontospravoar.com/feed",
+    "https://melhoresdestinos.com.br/feed",
+    "https://aeroin.net/feed",
+
+    # Conteúdo adicional que frequentemente publica promoções
+    "https://www.melhorescartoes.com.br/feed",
+    "https://www.mobills.com.br/blog/feed/",
+    "https://www.falandodeviagem.com.br/rss",
+
 ]
