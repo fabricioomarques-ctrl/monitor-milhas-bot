d2fc89bb1f15cd80be4ece39c8590df7cf100163
fabricioomarques-ctrl <fabricioo.marques@gmail.com>
2026-03-13 22:52:10 -0300

    Update sources.py

--- collectors/sources.py
+++ collectors/sources.py
@@ -1,16 +1,6 @@
-# Fontes RSS monitoradas pelo Radar de Milhas
-
 FONTES_RSS = [
-
-    # Sites principais do mercado de milhas
     "https://passageirodeprimeira.com/feed",
     "https://pontospravoar.com/feed",
-    "https://melhoresdestinos.com.br/feed",
+    "https://www.melhoresdestinos.com.br/feed",
     "https://aeroin.net/feed",
-
-    # Conteúdo adicional que frequentemente publica promoções
-    "https://www.melhorescartoes.com.br/feed",
-    "https://www.mobills.com.br/blog/feed/",
-    "https://www.falandodeviagem.com.br/rss",
-
 ]
