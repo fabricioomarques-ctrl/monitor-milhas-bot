6809454e996dd92d5d3a1bdfa383013893d4e8c5
fabricioomarques-ctrl <fabricioo.marques@gmail.com>
2026-03-13 23:56:40 -0300

    Delete collectors directory

--- collectors/sources.py
+++
@@ -1,6 +0,0 @@
-FONTES_RSS = [
-    "https://passageirodeprimeira.com/feed",
-    "https://pontospravoar.com/feed",
-    "https://www.melhoresdestinos.com.br/feed",
-    "https://aeroin.net/feed",
-]
